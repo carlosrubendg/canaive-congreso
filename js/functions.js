c57a2690eae558ffcea768bbd5a57ffab641fb76
Carlos Diaz <dg.carlosr@gmail.com>
2021-06-16 23:47:19 -0500

    Start project 06-16-21

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+// Desplazar a enlace ancla.
+
+const links = document.querySelectorAll(".ancla__btn");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
